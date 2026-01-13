90bf06ea59bc53c92e429004dd6e7aa4b97dde97
Belousov Oleg <belousov.oleg@gmail.com>
2026-01-13 16:29:30 +0300

    Minor improvements

--- src/dmr.c
+++ src/dmr.c
@@ -12,9 +12,23 @@
 #include "util.h"
 #include "cap.h"
 
-#define DMR_SYNC_DATA           0x40U
-#define DT_VOICE_LC_HEADER      0x01U
-#define DT_TERMINATOR_WITH_LC   0x02U
+typedef enum {
+    SYNC_VOICE  = 0x20,
+    SYNC_DATA   = 0x40
+} sync_t;
+
+typedef enum {
+    DT_PI_HEADER            = 0b0000,
+    DT_VOICE_LC_HEADER      = 0b0001,
+    DT_TERMINATOR_WITH_LC   = 0b0010,
+    DT_CSBK                 = 0b0011,
+    DT_MBC_HEADER           = 0b0100,
+    DT_MBC_CONTINUATION     = 0b0101,
+    DT_DATA_HEADER          = 0b0110,
+    DT_RATE_12_DATA         = 0b0111,
+    DT_RATE_34_DATA         = 0b1000,
+    DT_IDLE                 = 0b1001
+} data_type_t;
 
 static void get_rssi(uint8_t *data) {
     uint16_t raw = (data[0] << 8) + data[1];
@@ -30,23 +44,28 @@ void dmr_data(uint8_t *data, size_t size) {
         size -= 2;
     }
 
-    uint8_t type = data[0];
+    uint8_t sync = data[0] & 0xF0;
+    uint8_t type = data[0] & 0x0F;
 
-    if (type & DMR_SYNC_DATA) {
-        if (type & DT_VOICE_LC_HEADER) {
-            dump("Start", data, size);
-            lc_header_decode(data, size);
+    if (sync == SYNC_DATA) {
+        switch (type) {
+            case DT_VOICE_LC_HEADER:
+                dump("Start", data, size);
+                lc_header_decode(data, size);
 
-            cap_open(true);
-            cap_write(data, size);
-        }
+                cap_open(true);
+                cap_write(data, size);
+                break;
 
-        if (type & DT_TERMINATOR_WITH_LC) {
-            dump("End  ", data, size);
-            printf("\n");
+            case DT_TERMINATOR_WITH_LC:
+                dump("End  ", data, size);
+                printf("\n");
 
-            cap_write(data, size);
-            cap_close();
+                cap_write(data, size);
+                cap_close();
+
+            default:
+                break;
         }
     } else {
         dump("Pack ", data, size);
